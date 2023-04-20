832d599597e2b2a3eb173ce47f97a3954b840f10
marialoudiaz <loumariadiaz@gmail.com>
2023-04-20 12:13:57 +0200

    changes

---
+++ client/src/containers/Admin.js
@@ -0,0 +1,14 @@
+import React, { useState,useEffect } from "react";
+import {useParams} from 'react-router-dom'
+import axios from "axios";
+import { URL } from "../config";
+
+import React from 'react'
+
+function Admin() {
+  return (
+    <div>Admin</div>
+  )
+}
+
+export default Admin
