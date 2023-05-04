759da0d20f8626fd6af30661d6888798d38c1a1a
Pia Hildebrandt <3komma14@4dsl.de>
2023-05-04 12:11:58 +0200

    trying range

--- client/src/containers/RecruiterEdit.js
+++ client/src/containers/RecruiterEdit.js
@@ -120,6 +120,93 @@ function RecruiterEdit() {
     
   
   },[])
+        function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+          const [from, to] = getParsed(fromInput, toInput);
+          fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+          if (from > to) {
+              fromSlider.value = to;
+              fromInput.value = to;
+          } else {
+              fromSlider.value = from;
+          }
+      }
+          
+      function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+          const [from, to] = getParsed(fromInput, toInput);
+          fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+          setToggleAccessible(toInput);
+          if (from <= to) {
+              toSlider.value = to;
+              toInput.value = to;
+          } else {
+              toInput.value = from;
+          }
+      }
+
+      function controlFromSlider(fromSlider, toSlider, fromInput) {
+        const [from, to] = getParsed(fromSlider, toSlider);
+        fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+        if (from > to) {
+          fromSlider.value = to;
+          fromInput.value = to;
+        } else {
+          fromInput.value = from;
+        }
+      }
+
+      function controlToSlider(fromSlider, toSlider, toInput) {
+        const [from, to] = getParsed(fromSlider, toSlider);
+        fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+        setToggleAccessible(toSlider);
+        if (from <= to) {
+          toSlider.value = to;
+          toInput.value = to;
+        } else {
+          toInput.value = from;
+          toSlider.value = from;
+        }
+      }
+
+      function getParsed(currentFrom, currentTo) {
+        const from = parseInt(currentFrom.value, 10);
+        const to = parseInt(currentTo.value, 10);
+        return [from, to];
+      }
+
+      function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+          const rangeDistance = to.max-to.min;
+          const fromPosition = from.value - to.min;
+          const toPosition = to.value - to.min;
+          controlSlider.style.background = `linear-gradient(
+            to right,
+            ${sliderColor} 0%,
+            ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
+            ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
+            ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
+            ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
+            ${sliderColor} 100%)`;
+      }
+
+      function setToggleAccessible(currentTarget) {
+        const toSlider = document.querySelector('#toSlider');
+        if (Number(currentTarget.value) <= 0 ) {
+          toSlider.style.zIndex = 2;
+        } else {
+          toSlider.style.zIndex = 0;
+        }
+      }
+
+      const fromSlider = document.querySelector('#fromSlider');
+      const toSlider = document.querySelector('#toSlider');
+      const fromInput = document.querySelector('#fromInput');
+      const toInput = document.querySelector('#toInput');
+      fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+      setToggleAccessible(toSlider);
+
+      fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+      toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+      fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+      toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
 
 
 
@@ -139,9 +226,9 @@ function RecruiterEdit() {
       <p>location</p>
       <input id = 'location' value = {data['location']} onChange = {(e) =>changeData(e)}/>
       <div className = 'range-container'>
-        <div className = 'sliderscontrol'>
-          <input id="slider-1"  type="range" step="500" min='0' max='10000' defaultValue = '500' />
-          <input id="slider-2"  type="range" step="500" min='0' max='10000' defaultValue = '3000' />
+        <div className = 'sliders_control'>
+          <input id="fromSlider"  type="range" step="1" min='0' max='100' defaultValue = '10' />
+          <input id="toSlider"  type="range" step="1" min='0' max='100' defaultValue = '40' />
         </div>
         <div className="form_control">
           <div className="form_control_container">
